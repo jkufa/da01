4efeb8ce0f34e7f2c7c760fb394731746d311e1d
Jack <jhkmw3@mst.edu>
2019-02-04 20:40:52 -0600

    moved code into functions

--- p4.py
+++ p4.py
@@ -1,14 +1,11 @@
-a = int(input("enter triangle side 1: "))
-b = int(input("enter triangle side 2: "))
-c = int(input("enter triangle side 3: "))
+def isTriangle(a, b, c):
+    if (a + b) < c or (a + c) < b or (b + c) < a:
+        print("no")
+    else:
+        print("yes")
 
-#def isTriangle(a,b,c):
- #   if a + b > c or a + c > b or b + c > a:
- #       print("no")
- #   else:
- #       print("yes")
-
-if a + b < c or a + c < b or b + c < a:
-    print("no")
-else:
-    print("yes")
+def stickInput():
+    a = int(input("enter triangle side 1: "))
+    b = int(input("enter triangle side 2: "))
+    c = int(input("enter triangle side 3: "))
+    isTriangle(a, b, c)
