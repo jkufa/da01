47a4e71c44cab1f5a11541e8cc88cb7d32142976
Jack <jhkmw3@mst.edu>
2019-02-04 20:41:19 -0600

    remove non-deliverables

--- p3.py
+++ p3.py
@@ -1,13 +1,3 @@
-#string = input("enter a string: ")
-#print(string[1:] + string[0] + "ay")
-
-#string = input("enter a string: ")
-
 def pickWord(string):
-    head = string[0]
     newstring = string[1:] + string[0] + "ay"
     return newstring
-
-def main():
-    s = input("Enter a string: ")
-    print(newstring(s))
